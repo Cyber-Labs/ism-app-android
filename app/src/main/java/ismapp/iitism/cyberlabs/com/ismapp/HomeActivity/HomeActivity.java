eb9defd1413e59b4fe3a19edd3b5964eea1e42f1
amanatgithub <pandey.aman812@gmail.com>
2019-01-24 16:22:57 +0530

    Sign Up Layout

--- app/src/main/java/ismapp/iitism/cyberlabs/com/ismapp/HomeActivity/HomeActivity.java
+++ app/src/main/java/ismapp/iitism/cyberlabs/com/ismapp/HomeActivity/HomeActivity.java
@@ -1,28 +1,28 @@
 package ismapp.iitism.cyberlabs.com.ismapp.HomeActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-import com.appus.splash.Splash;
-
-import ismapp.iitism.cyberlabs.com.ismapp.MainActivity;
 import ismapp.iitism.cyberlabs.com.ismapp.R;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
-        Splash.Builder splash = new Splash.Builder(this, getSupportActionBar());
-        splash.setBackgroundImage(getResources().getDrawable(R.drawable.splash));
-        splash.setSplashImage(getResources().getDrawable(R.drawable.logo));
-        splash.perform();
+        setContentView(R.layout.verify_otp);
+//        LinearLayout ll=findViewById(R.id.ll);
+//        ll.setOnClickListener(null);
+//        Button button=findViewById(R.id.bt);
+//        button.setBackgroundColor(getColor(R.color.colorPrimary));
+//        button.setClickable(true);
+//        Splash.Builder splash = new Splash.Builder(this, getSupportActionBar());
+//        splash.setBackgroundImage(getResources().getDrawable(R.drawable.splash));
+//        splash.setSplashImage(getResources().getDrawable(R.drawable.logo));
+//        splash.perform();
     }
 
-    public void login(View view) {
-        startActivity(new Intent(this,MainActivity.class));
-    }
+ //   public void login(View view) {
+   //     startActivity(new Intent(this,MainActivity.class));
+   // }
 }
